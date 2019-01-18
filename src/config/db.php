c7740a491eefbce261bb27b1a149298860c31208
HunWalk <needforspeed623@gmail.com>
2019-01-18 18:56:31 +0100

    Added a template db configuration file

---
+++ src/config/db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;port=3306;dbname=timeclock_db',
+    'username' => '',
+    'password' => '',
+    'charset' => 'utf8',
+];
