6a10c9aab484d75e9a48f0e9a92739ff8e03b4d9
Hunwalk <needforspeed623@gmail.com>
2019-01-23 16:53:22 +0100

    Added an example db config file

---
+++ src/config/db.example.php
@@ -0,0 +1,10 @@
+<?php
+//Here you can edit your db connection.
+//Copy this file as db.php. Don't worry, that's gitignored
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;port:3306;dbname=timeclock',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
