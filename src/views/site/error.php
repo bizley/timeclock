7a5b0284e7d71478ec33c066bec163327c992f42
Bizley <pawel@positive.codes>
2019-01-29 13:37:46 +0100

    error page

--- src/views/site/error.php
+++ src/views/site/error.php
@@ -9,15 +9,13 @@ use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
-    <h1><?= Html::encode($this->title) ?></h1>
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+<div class="row">
+    <div class="col-lg-6">
+        <h1><?= Html::encode($this->title) ?></h1>
+        <div class="alert alert-danger">
+            <?= nl2br(Html::encode($message)) ?>
+        </div>
+        <p>The above error occurred while the Web server was processing your request.</p>
+        <p>Please contact us if you think this is a server error. Thank you.</p>
     </div>
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
 </div>
