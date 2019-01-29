a5ba42212313fca80632620211daf1e772194a09
Bizley <pawel@positive.codes>
2019-01-29 10:57:14 +0100

    login page

--- src/views/site/login.php
+++ src/views/site/login.php
@@ -4,60 +4,68 @@
 /* @var $loginModel app\models\LoginForm */
 /* @var $pinModel app\models\PinForm */
 
-use yii\helpers\Html;
 use yii\bootstrap4\ActiveForm;
+use yii\bootstrap4\Html;
 
 $this->title = Yii::t('app', 'Login');
 ?>
-<div class="form-group">
-    <h1><?= Yii::t('app', 'Login') ?></h1>
-</div>
+<div class="row align-items-end">
+    <div class="col-lg-8">
+        <div class="card shadow">
+            <div class="card-body">
+                <div class="float-right mb-5">
+                    <?= Html::a(Yii::t('app', 'New Account'), ['site/register'], ['class' => 'btn btn-outline-success btn-sm']) ?>
+                    <?= Html::a(Yii::t('app', 'Reset Password'), ['site/reset'], ['class' => 'btn btn-outline-secondary btn-sm']) ?>
+                </div>
+                <h3 class="card-title mb-5"><?= Yii::t('app', 'Login') ?></h3>
+                <?php $form = ActiveForm::begin([
+                    'layout' => 'horizontal',
+                    'fieldConfig' => [
+                        'horizontalCssClasses' => [
+                            'offset' => 'offset-sm-3',
+                            'label' => 'col-sm-3 text-center',
+                            'wrapper' => 'col-sm-9',
+                        ],
+                    ],
+                ]); ?>
 
-<?php $form = ActiveForm::begin([
-    'layout' => 'horizontal',
-    'fieldConfig' => [
-        'template' => "{label}\n<div class=\"col-sm-3\">{input}</div>\n<div class=\"col-sm-8\">{error}</div>",
-        'labelOptions' => ['class' => 'col-sm-1 control-label'],
-    ],
-]); ?>
-    <?= $form->field($loginModel, 'email')->textInput(['autofocus' => true]) ?>
-    <?= $form->field($loginModel, 'password')->passwordInput() ?>
-    <?= $form->field($loginModel, 'rememberMe')->checkbox([
-        'template' => "<div class=\"col-sm-offset-1 col-sm-3\">{input} {label}</div>\n<div class=\"col-sm-8\">{error}</div>",
-    ]) ?>
+                    <?= $form->field($loginModel, 'email')->textInput(['autofocus' => true]) ?>
+                    <?= $form->field($loginModel, 'password')->passwordInput() ?>
+                    <?= $form->field($loginModel, 'rememberMe', [
+                        'horizontalCssClasses' => [
+                            'label' => 'col-sm-9',
+                        ],
+                    ])->checkbox() ?>
 
-    <div class="form-group">
-        <div class="col-sm-offset-1 col-sm-11">
-            <?= Html::submitButton(Yii::t('app', 'Log In'), ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-            <?= Html::a(Yii::t('app', 'New Account'), ['site/register'], ['class' => 'btn']) ?>
-            <?= Html::a(Yii::t('app', 'Reset Password'), ['site/reset'], ['class' => 'btn text-muted']) ?>
+                    <div class="form-group text-center">
+                        <?= Html::submitButton(Yii::t('app', 'Log In'), [
+                            'class' => 'btn btn-primary btn-lg',
+                            'name' => 'login-button',
+                        ]) ?>
+                    </div>
+
+                <?php ActiveForm::end(); ?>
+            </div>
         </div>
     </div>
+    <div class="col-lg-4 mt-3">
+        <div class="card shadow">
+            <div class="card-body text-center">
+                <h3 class="card-title mb-5"><?= Yii::t('app', 'or PIN') ?></h3>
+                <?php $form = ActiveForm::begin(); ?>
 
-<?php ActiveForm::end(); ?>
+                    <?= $form->field($pinModel, 'pin')->label(false) ?>
+                    <?= $form->field($pinModel, 'rememberMe')->checkbox() ?>
 
-<hr>
+                    <div class="form-group text-center">
+                        <?= Html::submitButton(Yii::t('app', 'Log In'), [
+                            'class' => 'btn btn-primary btn-lg',
+                            'name' => 'login-button',
+                        ]) ?>
+                    </div>
 
-<div class="form-group">
-    <h1><?= Yii::t('app', 'or PIN') ?></h1>
-</div>
-
-<?php $form = ActiveForm::begin([
-    'layout' => 'horizontal',
-    'fieldConfig' => [
-        'template' => "{label}\n<div class=\"col-sm-3\">{input}</div>\n<div class=\"col-sm-8\">{error}</div>",
-        'labelOptions' => ['class' => 'col-sm-1 control-label'],
-    ],
-]); ?>
-<?= $form->field($pinModel, 'pin') ?>
-<?= $form->field($pinModel, 'rememberMe')->checkbox([
-    'template' => "<div class=\"col-sm-offset-1 col-sm-3\">{input} {label}</div>\n<div class=\"col-sm-8\">{error}</div>",
-]) ?>
-
-    <div class="form-group">
-        <div class="col-sm-offset-1 col-sm-11">
-            <?= Html::submitButton(Yii::t('app', 'Log In'), ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                <?php ActiveForm::end(); ?>
+            </div>
         </div>
     </div>
-
-<?php ActiveForm::end(); ?>
+</div>
